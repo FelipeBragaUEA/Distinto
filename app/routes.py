9ef1f331f1b52cd5f5071d85e8875c2479af1c96
FelipeBragaUEA <83717754+FelipeBragaUEA@users.noreply.github.com>
2024-05-29 14:53:50 -0400

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,10 @@ from models import Product
 import time
 import qrcode
 import os
-from PIL import Image
-import cv2
-import numpy as np
-import pyzbar.pyzbar as pyzbar
 
 def create_routes(app, blockchain):
     @app.route('/')
     def index():
-        return render_template('index.html')
-
-    @app.route('/register_product')
-    def register_product():
         return render_template('form.html')
 
     @app.route('/add_product', methods=['POST'])
@@ -55,26 +47,6 @@ def create_routes(app, blockchain):
         product_data = request.args.get('product_data')
         product = Product.from_json(product_data)
         return render_template('product_added.html', qr_code_path=qr_code_path, product=product)
-
-    @app.route('/scan_qr_code')
-    def scan_qr_code():
-        return render_template('scan_qr_code.html')
-
-    @app.route('/read_qr_code', methods=['POST'])
-    def read_qr_code():
-        file = request.files['qr_code']
-        file_path = os.path.join('static', 'uploaded_qrcodes', file.filename)
-        file.save(file_path)
-
-        # Decodificar o QR code
-        img = cv2.imread(file_path)
-        decoded_objects = pyzbar.decode(img)
-        if decoded_objects:
-            product_data = decoded_objects[0].data.decode('utf-8')
-            product = Product.from_json(product_data)
-            return render_template('product_info.html', product=product)
-        else:
-            return "No QR code found in the image.", 400
 
     @app.route('/chain', methods=['GET'])
     def get_chain():
